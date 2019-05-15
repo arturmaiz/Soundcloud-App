61f30f22eef48466be92eb7754176c60ab7df39e
Artur Maizelev <arturm0808@gmail.com>
2019-05-15 23:42:03 +0300

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-	return <div>App</div>;
-};
+import Layout from './Layout/Layout';
+import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Layout>
+					<BurgerBuilder />
+				</Layout>
+			</React.Fragment>
+		);
+	}
+}
 export default App;
